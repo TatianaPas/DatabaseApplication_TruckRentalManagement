c93ef812058a8dd237c483204a644b09b667d94a
Tatiana <tatjana.pasechnik@gmail.com>
2022-04-18 17:30:20 +1200

    Add project files.

---
+++ AssignmentProject_TatianaPasechnik/Models/DB/TruckEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssignmentProject_TatianaPasechnik.Models.DB
+{
+    public partial class TruckEmployee
+    {
+        public int EmployeeId { get; set; }
+        public string OfficeAddress { get; set; } = null!;
+        public string PhoneExtensionNumber { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
+
+        public virtual TruckPerson Employee { get; set; } = null!;
+    }
+}
---
+++ AssignmentProject_TatianaPasechnik/Models/DB/TruckRental.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssignmentProject_TatianaPasechnik.Models.DB
+{
+    public partial class TruckRental
+    {
+        public int RentalId { get; set; }
+        public int TruckId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public virtual TruckCustomer Customer { get; set; } = null!;
+        public virtual IndividualTruck Truck { get; set; } = null!;
+    }
+}
