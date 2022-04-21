8cd3d08c6446aaed70d762fd8474b360db8c184c
Tatiana <tatjana.pasechnik@gmail.com>
2022-04-21 17:04:48 +1200

    add customer, add employee form front End

---
+++ AssignmentProject_TatianaPasechnik/View/addCustomer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentProject_TatianaPasechnik.View
+{
+    /// <summary>
+    /// Interaction logic for addCustomer.xaml
+    /// </summary>
+    public partial class addCustomer : Window
+    {
+        public addCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
